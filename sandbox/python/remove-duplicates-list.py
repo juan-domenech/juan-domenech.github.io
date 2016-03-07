bece31f5a1bdf78e47d78b6d6b8882eae81fa13e
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-03-07 12:07:08 +0000

    improved using xrange()

--- sandbox/python/remove-duplicates-list.py
+++ sandbox/python/remove-duplicates-list.py
@@ -1,11 +1,11 @@
-# Given a list of integers or strings,remove all duplicates
-# The list might be unsorted and with one or none elements
+# Given a list of integers or strings, remove all duplicates.
+# The list might be unsorted and with one or none elements.
 # Example:
 # [5,1,2,3,4,5] = [1,2,3,4,5]
 # ['aaa','aaa','aab'] = ['aaa','aab']
 # [1] = [1]
 # [] = []
 
-def remove_duplicates(todo):
-	todo = sorted(todo)
-	return [ todo[item] for item in range(0,len(todo)) if todo[item:item+1] != todo[item+1:item+2] ]
+def remove_duplicates(work):
+	work = sorted(work)
+	return [ work[item] for item in xrange(0,len(work)) if work[item:item+1] != work[item+1:item+2] ]
